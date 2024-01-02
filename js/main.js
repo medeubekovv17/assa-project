69640ad53391584f4f643c0cc21cf0b62d7b6c6e
medeubekovv17 <200103154@stu.sdu.edu.kz>
2024-01-02 17:55:18 +0600

    update layouts

--- js/main.js
+++ js/main.js
@@ -1,27 +1,47 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const cards = document.querySelectorAll('.results__view-feedbacks-cards-card');
-
-  cards.forEach(card => {
-    card.addEventListener('click', () => {
-      // Переместите активную карточку в центр
-      cards.forEach(c => c.classList.remove('active'));
-      card.classList.add('active');
-
-      // Переместите остальные карточки вверх и вниз
-      cards.forEach(c => {
-        if (c !== card) {
-          if (c.offsetTop < card.offsetTop) {
-            c.classList.add('inactive-top');
-            c.classList.remove('inactive-bottom');
-          } else {
-            c.classList.add('inactive-bottom');
-            c.classList.remove('inactive-top');
-          }
-        }
-      });
-
-      // Переместите обертку карточек вверх и вниз
-      cardWrapper.style.transform = `translateY(-${card.offsetTop}px)`;
-    });
+  const burgerMenu = document.querySelector('.burger-menu');
+  const burgerIcon = document.querySelector('.burger-icon');
+  const headerLinksNav = document.querySelector('.header__mobile');
+  burgerMenu.addEventListener('click', function () {
+    headerLinksNav.classList.toggle('show-mobile');
+    burgerIcon.classList.toggle('show-mobile');
   });
 });
+
+$(document).ready(function(){
+    $('#mobFeedbacksSlider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+        adaptiveHeight: true
+    });
+
+    $('.results__view-mobFeedbacks-button.prev').click(function(){
+        $('#mobFeedbacksSlider').slick('slickPrev');
+    });
+
+    $('.results__view-mobFeedbacks-button.next').click(function(){
+        $('#mobFeedbacksSlider').slick('slickNext');
+    });
+});
+
+function moveToCenter(element) {
+    // Находим активную карточку и элементы inactive-top и inactive-bottom
+    const activeCard = document.querySelector('.results__view-feedbacks-cards-card.active');
+    const inactiveTop = document.querySelector('.results__view-feedbacks-cards-card.inactive-top');
+    const inactiveBottom = document.querySelector('.results__view-feedbacks-cards-card.inactive-bottom');
+
+    // Меняем классы для смены активной карточки
+    activeCard.classList.remove('active');
+    element.classList.add('active');
+
+    // Перемещаем активную карточку на место inactive-top или inactive-bottom
+    if (element.classList.contains('inactive-top')) {
+        inactiveTop.classList.remove('inactive-top');
+        activeCard.classList.add('inactive-top');
+    } else if (element.classList.contains('inactive-bottom')) {
+        inactiveBottom.classList.remove('inactive-bottom');
+        activeCard.classList.add('inactive-bottom');
+    }
+}
